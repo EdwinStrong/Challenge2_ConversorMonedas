fde87c2dde39fbcc144b14a6b8ed583d99fe0f64
Edwin Pacheco <edwinpacheco888@gmail.com>
2023-07-02 21:06:52 -0600

    refactor(Menu y input): Cambios en la estructura del joptionpaney variables definidas.

--- src/ejecutable/ConversorMain.java
+++ src/ejecutable/ConversorMain.java
@@ -1,50 +1,43 @@
 package ejecutable;
 
+import java.util.HashMap;
+
 import javax.swing.JOptionPane;
 
 import clases.MonedaValor;
-import clases.monedaException;
 
 
 public class ConversorMain {
+
 	public static void main(String[] args) {
-		MonedaValor conversion=new MonedaValor();
 
-		String[] opciones = {"Convertir Monedas", "Convertir Temperatura"};
-		String[] monedasName = {"Dolar", "Euros", "Libras", "Yen", "Won coreano"};
-
-		// Mostrar el cuadro de selección y obtener la opción seleccionada
-		String seleccion = (String) JOptionPane.showInputDialog(null, "Selecciona una opción de conversión",
-				"Menú", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+		String[] opcMenuPrincipal = {"Conversor de moneda", "Conversor de temperatura"};
 		
+		String seleccion1=(String) JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menu", JOptionPane.INFORMATION_MESSAGE,
+				null, opcMenuPrincipal, opcMenuPrincipal[0]);
 		
-		// Realizar la acción correspondiente a la opción seleccionada
-		if (seleccion != null) {
+		if(seleccion1==opcMenuPrincipal[0]) {//Convertir moneda
+			float dineroConvertir;
+			boolean exception=true;;
 			
-			if (seleccion.equals(opciones[0])) {//Moneadas
-				// Lógica para convertir monedas
-				JOptionPane.showMessageDialog(null, "Convertir Monedas");
-				
-				JOptionPane.showInputDialog("Ingresa la cantidad que deseas convertir: ", "0.00");//Valor numérico
-				 
+			while(exception) {
+				try {
+					//Solicitar cantidad de dinero a convertir.
+					dineroConvertir=Float.parseFloat(JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero que desea convertir: ", "Ingreso de datos",
+							JOptionPane.INFORMATION_MESSAGE, null, null, 0.00).toString());
+					exception=false;
+				}catch(NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Ingrese un dato numérico.");
+				}catch(NullPointerException e) {//Opción de cancelar
+					exception=false;
+				}
 			}
 			
-			else if (seleccion.equals(opciones[1])) {//Temperatura
-				// Lógica para convertir temperatura
-				JOptionPane.showMessageDialog(null, "Convertir Temperatura");
-			}
+			
+			
 		}
-
-		
-		/*
-		try {
-			JOptionPane.showMessageDialog(null, "El valor de la conversión de 100 mx son: "+conversion.convertirMoneda(5, 100)+" dólares.");
-		} catch (monedaException e) {
-			System.out.println("Error en conversion.");
-			e.printStackTrace();
+		else if(seleccion1==opcMenuPrincipal[1]) {//Convertir temperatura
+			
 		}
-
-		System.out.println("Funciona?");
-		*/
 	}
 }
